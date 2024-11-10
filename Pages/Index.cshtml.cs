6a3886c06d84fec7b8d8738ee31471151e1bc172
yaxley-peaks <epiclycoolgaemer@gmail.com>
2024-11-11 00:46:27 +0530

    initial commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EtyTool.Pages;
+
+public class IndexModel(ILogger<IndexModel> logger) : PageModel
+{
+    private readonly ILogger<IndexModel> _logger = logger;
+
+    public PageResult OnGet()
+    {
+        return Page();
+    }
+}
