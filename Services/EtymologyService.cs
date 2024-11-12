0dc87a4d3b28c227d763ab92e6bdfec6f9cddfd4
yaxley-peaks <epiclycoolgaemer@gmail.com>
2024-11-12 14:02:37 +0530

    works for all words now

--- Services/EtymologyService.cs
+++ Services/EtymologyService.cs
@@ -1,3 +1,5 @@
+using System.Text.Json;
+using System.Text.Json.Nodes;
 using EtyTool.Context;
 using EtyTool.Entities;
 using EtyTool.JsonSchema;
@@ -17,7 +19,7 @@ public class EtymologyService(
 {
     private readonly string _apiKey = config["Dictionary:MW:Key"]!;
 
-    private async Task<List<MerriamResponse>> GetDef(string word)
+    private async Task<List<(string,string)>> GetApiResponse(string word)
 
     {
         if (word.Contains(' '))
@@ -29,9 +31,30 @@ public class EtymologyService(
         HttpClient client = clientFactory.CreateClient(httpClientName!);
         var results = await client.GetAsync($"{word}?key={_apiKey}");
         var contents = await results.Content.ReadAsStringAsync();
-        var json = MerriamResponse.FromJson(contents);
+       JsonNode? j =  JsonSerializer.Deserialize<JsonNode>(contents);
+       // j[n]["et"][0][1].ToString();
+       List<(string, string)> returnList = [];
+       if (returnList == null) throw new ArgumentNullException(nameof(returnList));
+       foreach (var node in j!.AsArray())
+       {
+           try
+           {
+               if (node?["et"] == null)
+               {
+                   continue;
+               }
 
-        return await Task.FromResult(json);
+               if (node["et"]?[0]?[0]?.ToString() == "text")
+               {
+                  returnList.Add((word, node["et"]?[0]?[1]?.ToString())!); 
+               }
+           }
+           catch (NullReferenceException)
+           {
+           }
+       }
+
+       return returnList;
     }
 
     private void CacheEtymology(EtymologyDto eDto)
@@ -51,7 +74,7 @@ public class EtymologyService(
                 select x);
             if (etymologyResultSet.Count() == 1)
             {
-                var eId = (from e in etymologyResultSet select e.Id).First()!;
+                var eId = (from e in etymologyResultSet select e.Id).First();
                 w.Etymology = eId;
             }
             else
@@ -65,7 +88,7 @@ public class EtymologyService(
             ctx.Add(w);
             ctx.SaveChanges();
         }
-        catch (Exception e)
+        catch (Exception)
         {
             // ignored
         }
@@ -84,9 +107,8 @@ public class EtymologyService(
         }
         else
         {
-            var x = GetDef(word);
-            x.Wait();
-            MerriamResponse mr = x.Result.First()!;
+            var etymologies = GetApiResponse(word);
+            etymologies.Wait();
             /*
              * Etymology is of the form
              * et: [
@@ -96,17 +118,13 @@ public class EtymologyService(
              * So we must drop all the rest. we only want the one
              * with "text"
              */
-            var e = (from et
-                            in mr.Et
-                        where et[0] == "text"
-                        select et[1])
-                    .FirstOrDefault()
-                ;
+            var e = etymologies.Result.First().Item2;
+
+                    
 
             // Remove words like "middle" or "ancient"
-            e = string.Concat(e?.Split(' ').First(x => x is not ("Middle" or "Ancient" or "Old" or "New" or "Modern"))!
-                                  .TakeWhile(IsAsciiLetter)
-                              ?? Array.Empty<char>()).ToLowerInvariant();
+            e = string.Concat(e.Split(' ').First(x => x is not ("Middle" or "Ancient" or "Old" or "New" or "Modern"))
+                .TakeWhile(IsAsciiLetter)).ToLowerInvariant();
             eDto.Word = word.ToUpperInvariant();
             eDto.OriginLanguage = e.ToUpperInvariant();
             CacheEtymology(eDto);
